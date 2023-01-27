b8fdbf6d3f3e7d94ce7f7d2bc4e2c06418d14d3d
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2023-01-27 11:54:26 +0000

    added chapter02

---
+++ chapter02/src/App.2.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+import {TableBox} from './components/Table';
+
+const API_URL = 'https://cog-study.herokuapp.com';
+const PATH = '/react_level4/chapter02/player/';
+
+function App() {
+  return (
+    <div className="App">
+      <Title>プレイヤー一覧</Title>
+      <PlayerList />
+    </div>
+  );
+}
+
+const PlayerList = () => {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    (async() => {
+      const response = await fetch(API_URL + PATH, {method : 'GET'});
+      const data = await response.json();
+      setPlayers(data);
+    })();
+  },[]);
+  
+  return (
+    <div>
+      {players.length > 0 && <TableBox data={players} />}
+    </div>
+  );
+};
+
+const Title = (props) => <h1>{props.children}</h1>;
+
+export default App;
---
+++ chapter02/src/App.4.js
@@ -0,0 +1,99 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+import { TableBox } from './components/Table';
+import { Detail } from './components/Detail';
+import { Form } from './components/Form';
+
+const API_URL = 'https://cog-study.herokuapp.com';
+const PATH = '/react_level4/chapter02/player/';
+
+function App() {
+  return (
+    <div className="App">
+      <Title>プレイヤー登録</Title>
+      <PlayerForm />
+      <hr />
+      <PlayerList />
+    </div>
+  );
+}
+
+const PlayerForm = () => {
+  const [player, setPlayer] = useState({
+    name : '',
+    hp : '',
+    mp : '',
+    job : '',
+  });
+  
+  const handleOnChange = (e) => {
+    const p = {...player};
+    p[e.target.id] = e.target.value;
+    setPlayer(prevP => p);
+  };
+  
+  const handleOnClickButton = (e) => {
+    (async () => {
+      const requestOptions = {
+        method: 'post',
+  	    body: JSON.stringify(player),
+  	    headers: {'Content-Type': 'application/json'},
+      };
+      const response = await fetch(API_URL + PATH, requestOptions);
+      const data = await response.json();
+      console.log(data);
+      
+      setPlayer({
+        name : '',
+        hp : '',
+        mp : '',
+        job : '',
+      });
+    })();
+  };
+  
+  return (
+    <Form
+      player={player}
+      handleOnChange={handleOnChange}
+      handleOnClickButton={handleOnClickButton}
+    />
+  );
+};
+
+const PlayerList = () => {
+  const [players, setPlayers] = useState([]);
+  
+  const fetchPlayerList = async () => {
+    const response = await fetch(API_URL + PATH, {method : 'GET'});
+    const data = await response.json();
+    return data;
+  };
+  
+  const getPlayerList = async () => {
+    const p = await fetchPlayerList();
+    setPlayers(p);
+  };
+  
+  useEffect(() => {
+    getPlayerList();
+  },[]);
+  
+  const handleOnClick = (e) => {
+    getPlayerList();
+  };
+  
+  return (
+    <div>
+      <div className="btnUpdate">
+        <button onClick={handleOnClick}>一覧更新</button>
+      </div>
+      {players.length > 0 && <TableBox data={players} />}
+    </div>
+  );
+};
+
+const Title = (props) => <h1>{props.children}</h1>;
+
+export default App;
