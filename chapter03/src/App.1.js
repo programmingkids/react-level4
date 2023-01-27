b006e5d085d8ec9555406e8d764d83ed166a5f1c
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2023-01-27 12:04:38 +0000

    added chapter03

---
+++ chapter03/src/App.1.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import {TableBox} from './components/Table';
+
+const API_URL = 'https://cog-study.herokuapp.com';
+const PATH = '/react_level4/chapter03/enemy/';
+
+function App() {
+  return (
+    <div className="App">
+      <Title>敵一覧</Title>
+      <EnemyList />
+    </div>
+  );
+}
+
+const EnemyList = () => {
+  const [enemies, setEnemies] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_URL + PATH)
+    .then(res => res.data)
+    .then(data => setEnemies(data));
+  },[]);
+  
+  return (
+    <div>
+      {enemies.length > 0 && <TableBox data={enemies} />}
+    </div>
+  );
+};
+
+const Title = (props) => <h1>{props.children}</h1>;
+
+export default App;
